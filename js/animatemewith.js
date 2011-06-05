b54e4759feba70eec15d1f03143938123c141054
Robert Brodersen <rbtbr@me.com>
2011-06-05 08:02:06 +0000

    major script.js change

--- js/animatemewith.js
+++ js/animatemewith.js
@@ -1,56 +1,56 @@
-$(document).ready(function () {
+/*jshint jquery: true, browser: true, bitwise: true, curly: true, eqeqeq: true, noempty: true, onevar: true, plusplus: true, undef: true, strict: true, white: true */
 
-    function leseURL() {
-        var seitenURL = document.URL;
-        var myMatch = seitenURL.match(/#(.*)/);
-        if (myMatch !== null) {
-            argumentURL = myMatch[1];
-        }
-    }
-
-    leseURL();
-
-    if (argumentURL !== 'leer') {
-        $('#' + argumentURL).addClass('active').fadeIn('slow', function () {
-            $('nav li a').each(function () {
-                if (this.href.match(/#(.*)/)[1] === argumentURL) {
-                    $(this).parent().addClass('active');
-                }
-            });
+function handleHash() {
+    "use strict";
+    var urlHash = location.hash;
+    if (urlHash !== "") {
+        $(urlHash).addClass('active').fadeIn('slow');
+        $('nav li a').each(function () {
+            if ($(this).attr('href') === urlHash) {
+                $(this).parent().addClass('active');
+            }
         });
     }
+}
 
-    // ... und her mit dem neuen
-    function showItem(clickedPanel) {
-        $('#' + clickedPanel).addClass('active').fadeIn('slow', function () {
-            $('footer').fadeIn('slow');
-        });
+// ... und her mit dem neuen
+function showItem(clickedPanel) {
+    "use strict";
+    $('#' + clickedPanel).addClass('active').fadeIn('slow', function () {
+        $('footer').fadeIn('slow');
+    });
+}
+
+// Weg mit dem Alten ...
+function blendeAus(clickedPanel) {
+    "use strict";
+    switch (argumentURL) {
+    case clickedPanel:
+        return;
+    case 'leer':
+        showItem(clickedPanel);
+        break;
     }
-
-    // Weg mit dem Alten ...
-    function blendeAus(clickedPanel) {
-        leseURL();
-        switch (argumentURL) {
-        case clickedPanel:
-            return;
-        case 'leer':
+    $('footer').fadeOut('slow', function () {
+        $('#' + argumentURL).removeClass('active').fadeOut('slow', function () {
             showItem(clickedPanel);
-            break;
-        }
-        $('footer').fadeOut('slow', function () {
-            $('#' + argumentURL).removeClass('active').fadeOut('slow', function () {
-                showItem(clickedPanel);
-            });
         });
-    }
+    });
+}
 
-    // Hmm, navigiere, navigiere ...
+$(document).ready(function () {
+    "use strict";
+    
+    // Ist die Seite mit einem Hash aufgerufen worden?
+    handleHash();
+    
+    // Navigiere, navigiere ...
     $('nav ul li').click(function () {
         $('nav li').removeClass('active');
         $(this).addClass('active');
-        blendeAus(this.href.match(/#(.*)/)[1]);
+        blendeAus(this.href);
     });
-
+    
     // Target _blank jQuery-Style
     $('article ul li a').click(function () {
         window.open(this.href);
